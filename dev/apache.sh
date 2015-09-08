d8217cf41657bdead8d034635c8128553cd91589
Ricardo <ricardo.perez@manoderecha.mx>
2015-09-08 10:28:51 -0500

    add apache & mysql

---
+++ dev/apache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "INSTALL APACHE"
+sudo apt-get update
+sudo apt-get install apache2 libapache2-mod-php5 php5-mcrypt -y
+echo "FINISH INSTALL APACHE2"
+
---
+++ dev/mysql.sh
@@ -0,0 +1,3 @@
+echo "INSTALL MYSQL"
+sudo apt-get install mysql-server php5-mysql -y
+echo "FINISH INSTALL MYSQL"
