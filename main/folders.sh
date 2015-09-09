a4f914bf7ffa6ea810469fa2133f4c6b0b033e8c
Ricardo <ricardo.pcan@gmail.com>
2015-09-09 18:54:59 -0500

    add folders

---
+++ main/folders.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "CREATE ENVIRONMENT FOLDERS"
+cd
+mkdir -p mywork/cgfie/projects
+mkdir myjunk
+mkdir mymusic
+mkdir myimages
+mkdir myreferences
+mkdir myinbox
+mkdir myvideos
+mkdir myprojects
+
+cd ~/mywork/cgfie/projects
+wd add cgfie_p
+cd ~/myinbox
+wd add inbox
+cd ~/myprojects
+wd add projects
+
