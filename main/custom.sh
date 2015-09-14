8e22160047bd406ea0970581543f2e945d9964c4
Ripper <ricardo.pcan@gmail.com>
2015-09-13 23:36:59 -0500

    add gitconfig

--- main/custom.sh
+++ main/custom.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "CUSTOMIZATION FILES"
+rm ~/.zshrc.sh
 sudo ln -s ~/my-ubuntu-post-install/main/customFiles/.zshrc ~/
 mkdir ~/.i3
 sudo ln -s ~/my-ubuntu-post-install/main/customFiles/config ~/.i3/
+rm ~/.gitconfig
+sudo ln -s ~/my-ubuntu-post-install/main/customFiles/.gitconfig ~/
+
 
 #Alias
 
@@ -12,3 +16,5 @@ git config --global alias.mr merge
 git config --global alias.ci commit
 git config --global alias.ps push
 git config --global alias.ft fetch
+
+chsh -s /bin/zsh
